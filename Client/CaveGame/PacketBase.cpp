a7fb81ee208521dc25816d90ca7ff2f66cca7ab8
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-11-07 17:43:18 +0900

    Descript PacketBase

--- Client/CaveGame/PacketBase.cpp
+++ Client/CaveGame/PacketBase.cpp
@@ -2,6 +2,8 @@
 #include "PacketBase.hpp"
 #include "Hero.h"
 // s2c를 정의 하는 CPP
+// Server -> Client , 서버로 부터의 패킷을 받아서 처리하는 함수들의 모임
+// c2s는 없으면 링크에러나서 더미로 만들었음 좋은 의견있으면 건의 부탁
 
 #define DECLARE_PACKET_FUNC(pkt_name) void pkt_name::Handle(const uint64 id, const pkt_name& pkt_) noexcept        
 
